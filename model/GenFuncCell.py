ba3d5498ae8b7ab56da3a5315d58071188200b6b
Harry Bleyan <gbleyan@gmail.com>
2017-02-16 08:53:23 -0500

    Debug

--- model/GenFuncCell.py
+++ model/GenFuncCell.py
@@ -246,10 +246,9 @@ class GenFuncCell(RNNCell):
              stddev=.01,
              bias=0,
              name=''):
-        if in_layer.get_shape().as_list()[-1] == 784:
-            in_shape = 1
-        else:
-            in_shape = in_layer.get_shape().as_list()[-1]
+        in_shape = in_layer.get_shape().as_list()[-1]
+
+        print(in_layer.get_shape().as_list(), 'CONV SHAPE')
 
         if isinstance(ksize, int):
             ksize1 = ksize
